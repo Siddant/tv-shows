a9392d0974ac481bfb09fcd4b8c19fb028cf4d50
Siddant <siddantgurung@hotmail.co.uk>
2019-02-04 16:21:09 +0000

    build a updated version

--- index.js
+++ index.js
@@ -8,7 +8,7 @@ const routes = require('./config/routes')
 
 const app = express()
 
-mongoose.connect('mongodb://localhost/tv-shows-db')
+mongoose.connect(process.env.MONGODB_URI)
 
 app.use(express.static(`${__dirname}/dist`))
 
