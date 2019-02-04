b4d5687fe3b58a2ef94a8d24467f732510b1d3c8
Siddant <siddantgurung@hotmail.co.uk>
2019-02-04 15:58:27 +0000

    connected to dist

--- index.js
+++ index.js
@@ -8,10 +8,11 @@ const routes = require('./config/routes')
 
 const app = express()
 
-
 mongoose.connect('mongodb://localhost/tv-shows-db')
+
+app.use(express.static(`${__dirname}/dist`))
 
 app.use(bodyParser.json())
 app.use('/api',routes)
 
-app.listen(process.env.PORT, () => console.log('Express is listening on port 4000'))
+app.listen(process.env.PORT, () => console.log(`Express is listening on port ${process.env.PORT}`))
